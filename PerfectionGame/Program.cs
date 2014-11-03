b131e1bff763798c990fdf851c2e112faa3decb5
mfharboe <mfharboe@hotmail.com>
2014-11-03 10:50:46 +0100

    This is first commit

---
+++ PerfectionGame/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfectionGame
+{
+    public class Program
+    {
+        static List<int> m_list = new List<int>();
+        static int sizeOfList = 50;
+
+        static void Main(string[] args)
+        {
+            CreateList();
+            //FindConsecutiveNumbers();
+            PrintOutConsecutiveNumber();
+
+        }
+        public static void CreateList()
+        {
+            Random random = new Random();
+            m_list.Add(14);
+            m_list.Add(17);
+            m_list.Add(19);
+            //m_list.Add(3);
+            //m_list.Add(4);
+            //m_list.Add(7);
+            //m_list.Add(8);
+            //m_list.Add(10);
+            //m_list.Add(4);
+            //m_list.Add(16);
+            //m_list.Add(17);
+            //m_list.Add(16);
+            //for (int i = 0; i < sizeOfList; i++)
+            //{
+            //    m_list.Add(random.Next(0, 20));
+            //}
+        }
+
+
+        public static void PrintOutConsecutiveNumber()
+        {
+            List<int> sortedList = Consecutive(m_list);
+            foreach (var item in sortedList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        //internal static List<int> Consecutive(List<int> startList)
+        //{
+        //    List<int> sortedList = new List<int>();
+        //    List<List<int>> tempList = new List<List<int>>();
+        //    List<int> finalList = new List<int>();
+        //    if (startList == null)
+        //    {
+        //        return new List<int>();
+        //    }
+        //    if (startList.Count == 1)
+        //    {
+        //        return startList;
+        //    }
+
+        //    for (int i = 0; i < startList.Count - 1; i++)
+        //    {
+        //        if (i != startList.Count)
+        //        {
+                   
+        //            if (startList[i] + 1 == startList[i + 1])
+        //            {
+
+        //                sortedList.Add(startList[i]);
+        //                sortedList.Add(startList[i + 1]);
+        //            }
+        //            else
+        //            {
+        //                if(sortedList.Count != 2)
+        //                tempList.Add(sortedList);
+        //                sortedList = new List<int>();
+        //            }
+        //            if (startList.Count - 2 == i)
+        //            {
+        //                tempList.Add(sortedList);
+        //            }
+        //        }
+        //    }
+
+        //    if (tempList.Count == 1)
+        //    {
+
+        //        return tempList[0];
+        //    }
+        //    else
+        //    {
+        //        int count = 0;
+        //        foreach (var item in tempList)
+        //        {
+        //            if (item.Count > count)
+        //            {
+        //                count = item.Count;
+        //                finalList = item;
+        //            }
+
+        //        }
+        //    }
+
+        //    for (int i = 0; i < finalList.Count - 1; i++)
+        //    {
+        //        if (finalList[i] == finalList[i + 1])
+        //        {
+        //            finalList.RemoveAt(i);
+        //        }
+                
+        //    }
+        //    //Console.WriteLine(tempList.Count);
+        //    return finalList;
+        //}
+        internal static List<int> Consecutive(List<int> testArray)
+        {
+            if (testArray == null)
+            {
+                return new List<int>();
+            }
+            var listOfLists = new List<List<Int32>>();
+            var resultList = new List<Int32>();
+            var finalResult = new List<int>();
+            listOfLists.Add(resultList);
+
+            for (var i = 0; i < testArray.Count; i++)
+            {
+                if (i == 0)
+                {
+                    resultList.Add(testArray[0]);
+                }
+                else if (resultList.Last().Equals(testArray[i] - 1))
+                {
+                    resultList.Add(testArray[i]);
+                }
+                else
+                {
+                    resultList = new List<Int32>();
+                    listOfLists.Add(resultList);
+                    resultList.Add(testArray[i]);
+                }
+            }
+            var count = 0;
+            foreach (var list in listOfLists)
+            {
+
+                if (count < list.Count)
+                {
+                    finalResult = null;
+                    finalResult = list;
+                    count = list.Count;
+                }
+            }
+            return finalResult;
+        }
+    }
+}
---
+++ PerfectionGame/TestArray.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace PerfectionGame
+{
+    [TestFixture]
+    static class TestArray
+    {
+       
+        [SetUp]
+        public static void SetUp()
+        {
+            
+            Console.WriteLine("Setup called");
+        }
+        public static void TearDown()
+        {
+            Console.WriteLine("TearDown called");
+        }
+        
+        [Test]
+        public static void TestGroupConsecutives()
+        {
+            List<int> startList = new List<int> { 1, 2, 3, 8, 3, 5,6,8,8,6,7,8,9,10,11,33,44,12 };
+            List<int> endArray = new List<int>();
+            endArray = Program.Consecutive(startList);
+            List<int> correctList = new List<int> { 6,7,8,9,10,11 };
+            CollectionAssert.AreEqual(correctList, endArray);
+  
+        }
+        [Test]
+        public static void TestStig()
+        {
+            List<int> startList = new List<int> { 7,5,17,18,19,1,3,27 };
+            List<int> endArray = new List<int>();
+            endArray = Program.Consecutive(startList);
+            List<int> correctList = new List<int> { 17,18,19 };
+            CollectionAssert.AreEqual(correctList, endArray);
+
+        }
+        [Test]
+        public static void TestSmallConsecutives()
+        {
+            List<int> startList = new List<int> {1,2};
+            List<int> endArray = new List<int>();
+            endArray = Program.Consecutive(startList);
+            List<int> correctList = new List<int> {1,2};
+            CollectionAssert.AreEqual(correctList, endArray);
+
+        }
+        [Test]
+        public static void TestLastCon()
+        {
+            List<int> startList = new List<int> { 1, 2,3,4,5,6,50 };
+            List<int> endArray = new List<int>();
+            endArray = Program.Consecutive(startList);
+            List<int> correctList = new List<int> { 1, 2, 3, 4 , 5,6};
+            CollectionAssert.AreEqual(correctList, endArray);
+
+        }
+        [Test]
+        public static void TestfirstCon()
+        {
+            List<int> startList = new List<int> { 4, 1, 2,3 };
+            List<int> endArray = new List<int>();
+            endArray = Program.Consecutive(startList);
+            List<int> correctList = new List<int> { 1, 2, 3 };
+            CollectionAssert.AreEqual(correctList, endArray);
+        }
+        [Test]
+        public static void TestOneInt()
+        {
+            List<int> startList = new List<int> { 1 };
+            List<int> endArray = new List<int>();
+            endArray = Program.Consecutive(startList);
+            List<int> correctList = new List<int> { 1 };
+            CollectionAssert.AreEqual(correctList, endArray);
+        }
+        [Test]
+        public static void TestForNull()
+        {
+            List<int> startList = null;
+            List<int> endArray = new List<int>();
+            endArray = Program.Consecutive(startList);
+            List<int> correctList = new List<int>();
+            CollectionAssert.AreEqual(correctList, endArray);
+        }
+
+        [Test]
+        public static void Test1()
+        {
+            List<int> startList = new List<int> { 1,2,3 };
+            List<int> endArray = new List<int>();
+            endArray = Program.Consecutive(startList);
+            List<int> correctList = new List<int> { 1, 2, 3 };
+            CollectionAssert.AreEqual(correctList, endArray);
+        }
+
+        [Test]
+        public static void Test2()
+        {
+            List<int> startList = new List<int> { 1, 2, 7,9,10 };
+            List<int> endArray = new List<int>();
+            endArray = Program.Consecutive(startList);
+            List<int> correctList = new List<int> { 1,2 };
+            CollectionAssert.AreEqual(correctList, endArray);
+        }
+
+        [Test]
+        public static void Test3()
+        {
+            List<int> startList = new List<int> { 5,4,3,2,1};
+            List<int> endArray = new List<int>();
+            endArray = Program.Consecutive(startList);
+            List<int> correctList = new List<int> { 5 };
+            CollectionAssert.AreEqual(correctList, endArray);
+        }
+
+        [Test]
+        public static void Test4()
+        {
+            List<int> startList = new List<int> { 14, 17, 19 };
+            List<int> endArray = new List<int>();
+            endArray = Program.Consecutive(startList);
+            List<int> correctList = new List<int> { 14 };
+            CollectionAssert.AreEqual(correctList, endArray);
+        }
+    }
+}
